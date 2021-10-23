0b0bda727d569c16c20bcf909db08b120044298c
AHpx <AHpx@yandex.com>
2021-10-24 00:08:57 +0800

    Initialized project

---
+++ GenshinKin.Runtime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenshinKin.Runtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
